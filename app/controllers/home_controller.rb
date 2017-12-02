a65efca84cdb34414e95cd8643d62372f5534344
Albertto <albertto.castro@gmail.com>
2017-12-02 17:41:30 -0300

    Nationalities advancements made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def cultures
+    @nationalities = Nationality.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get "/cultures" => "home#cultures"
+
   resources :addresses
+
+  resources :nationalities
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
