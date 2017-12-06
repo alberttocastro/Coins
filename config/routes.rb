ac0426738215169a64906d5dd6662a4daa413c54
Albertto <albertto.castro@gmail.com>
2017-12-06 09:58:43 -0300

    Publishers controller made

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   resources :telephones
 
   resources :groups
+
+  resources :publishers
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
