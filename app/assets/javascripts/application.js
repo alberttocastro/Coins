34f78a0eb6eeacad1dc215431f0dd76f5c3e3667
Alberto C Bandeira <albertto.castro@gmail.com>
2018-04-28 12:42:50 -0300

    Atualizando javascript e problemas com atualização de pagina

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,63 @@
 //= require jquery
 //= require jquery_ujs
 // Sempre inclua
-//= require turbolinks
+// require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+
+
+$(document).ready(function(){
+
+    console.log("Passei aqui");
+
+    $('select').formSelect();
+
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: false,
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+      });
+    $('.button-collapse').sidenav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+    onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+    onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+      }
+    );
+    
+    $('.fixed-action-btn').floatingActionButton({
+      direction: 'top', // Direction menu comes out
+      hoverEnabled: true, // Hover enabled
+      toolbarEnabled: false // Toolbar transition enabled
+    });
+    
+      $('.sidenav').sidenav();
+
+      $("#dropdown-trigger").dropdown({
+        hover: true,
+        coverTrigger: false,
+        beloworigin: true
+        });
+
+    // Sets the active item in the navbar extended
+    var pathname = window.location.pathname;
+    var pathparts = pathname.split("/");
+    if (pathparts[1] != null) {
+        $("#".concat(pathparts[1])).addClass("active");
+    }
+});
+
+//$(document).ready(runfunction);
+//$(document).on('page:load', runfunction);
+//$(document).on('page:change', runfunction);
+//$(window).bind('page:change', runfunction);
+//$(document).onload = runfunction;
+//window.onload = runfunction;
+
