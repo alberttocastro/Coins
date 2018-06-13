5fa36299621a84c9ee5d9c77a7a64a4bc736abe3
Alberto C Bandeira <albertto.castro@gmail.com>
2018-06-12 22:22:19 -0300

    Home

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,3 @@
-var app = angular.module('mainModule', []);
-
-app.controller('mainController', function($scope, $http){ //o scope liga o js e o template
-  $scope.nome = 'Valor Inicial';
-  //$http.get().success();
-  $scope.reset = function()
-  {
-    $scope.nome = '';
-  }
+$(document).ready(function(){
+    $('.sidenav').sidenav();
 });
