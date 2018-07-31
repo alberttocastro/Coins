ceff96e127dd1bff1280a578c0d1ca72313fe523
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-07-31 10:23:42 -0300

    Ações do controlador e registro de rotas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,29 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [:cultures, :characteristics]
+  #before_action :authenticate_user!, only: [:cultures, :characteristics]
   def index
     @navbar = "partials/navbar-home"
     render "index"
   end
 
-  def cultures
-    @nationalities = Nationality.all
-    @idioms = Idiom.all
+  def overview
+    @navbar = "partials/navbar-overview"
+    render "overview"
   end
 
-  def characteristics
-    @macroregions = Macroregion.all
-    @adrstypes = Adrstype.all
+  def assignments
+    @navbar = "partials/navbar-assignments"
+    render "assignments"
   end
-  
+
 end
+
+
+  # def cultures
+  #   @nationalities = Nationality.all
+  #   @idioms = Idiom.all
+  # end
+
+  # def characteristics
+  #   @macroregions = Macroregion.all
+  #   @adrstypes = Adrstype.all
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', confirmations: 'users/confirmations', passwords: 'users/passwords', unlocks: 'users/unlocks'}
 
   root 'home#index'
+  get 'overview', to: "home#overview", as: :overview
+  get 'assignments', to: "home#assignments", as: :assignments
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
