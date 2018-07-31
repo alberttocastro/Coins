8cde1b11af565a2a9153be0d8a06fd1103df49e8
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-07-31 10:07:40 -0300

    Refatorando

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,52 +21,52 @@
 
 
 $(document).ready(function(){
-
-    console.log("Passei aqui");
-
-    $('.modal').modal();
-
-    $('select').formSelect();
-
-    $('.datepicker').datepicker({
-        format: 'yyyy-mm-dd',
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: false,
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-      });
-    $('.button-collapse').sidenav({
+  
+  console.log("Passei aqui");
+  
+  $('.modal').modal();
+  
+  $('select').formSelect();
+  
+  $('.datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: false,
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
+  
+  $('.button-collapse').sidenav({
     menuWidth: 300, // Default is 300
     edge: 'left', // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true, // Choose whether you can drag to open on touch screens,
     onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
     onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
-      }
-    );
-    
-    $('.fixed-action-btn').floatingActionButton({
-      direction: 'top', // Direction menu comes out
-      hoverEnabled: true, // Hover enabled
-      toolbarEnabled: false // Toolbar transition enabled
-    });
-    
-    $(".button-collapse").sideNav();
-
-      $("#dropdown-trigger").dropdown({
-        hover: true,
-        coverTrigger: false,
-        beloworigin: true
-        });
-
-    // Sets the active item in the navbar extended
-    var pathname = window.location.pathname;
-    var pathparts = pathname.split("/");
-    if (pathparts[1] != null) {
-        $("#".concat(pathparts[1])).addClass("active");
-    }
+  });
+  
+  $('.fixed-action-btn').floatingActionButton({
+    direction: 'top', // Direction menu comes out
+    hoverEnabled: true, // Hover enabled
+    toolbarEnabled: false // Toolbar transition enabled
+  });
+  
+  $(".button-collapse").sideNav();
+  
+  $("#dropdown-trigger").dropdown({
+    hover: true,
+    coverTrigger: false,
+    beloworigin: true
+  });
+  
+  // Sets the active item in the navbar extended
+  var pathname = window.location.pathname;
+  var pathparts = pathname.split("/");
+  if (pathparts[1] != null) {
+    $("#".concat(pathparts[1])).addClass("active");
+  }
 });
 
 //$(document).ready(runfunction);
