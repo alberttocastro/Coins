dce2d8e04fdfa39af3692a5526d5bb75a51042a8
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-08-27 21:04:47 -0300

    Destruir todos as confirmações de endereço feito ao concluir designação

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,26 @@
 class Assignment < ApplicationRecord
 
+  belongs_to :card
+
     # A verificação de concluido se dá de acordo com a data
     def concluded?
       !self.date.nil?
+    end
+
+    # Retorna um array com todos os endereços que foram prontos nessa designação
+    def all_done
+      @array = []
+      AssignmentAddress.where(assignment_id: self.id).each do |assignment_address|
+        @array << assignment_address.address_id
+      end
+
+      return @array
+    end
+
+    # Sempre depois que houver o commit, caso a data tenha sido preenchida, ele destroi todas as informações de endereços que já foram feitos
+    after_commit do
+      if self.date_changed? && !self.date.nil?
+        AssignmentAddress.where(assignment_id: self.id).destroy
+      end
     end
 end
