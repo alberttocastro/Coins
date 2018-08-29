42edc54bde079dacafd505261dc5dec5de3bae10
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-08-28 21:57:31 -0300

    Método de retornar os endereços para não voltar Método para retornar as designações não completas

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Retorna todos os endereços que não devem ser batidos de acordo com a deisgnação da pessoa, no seguinte formato:
+  # ["Macroregião",[Endereço 1, Endereço 2, Endereço 3]]
+  def do_not_call_info
+    @answer = []
+
+    @macroregions = []
+    self.assignments_to_do.each do |assignment|
+      assignment.card.addresses.each do |address|
+        @answer << address.macroregion_id
+      end
+    end
+
+    @macroregions = @macroregions.uniq
+
+    @macroregions.each do |macroregion_id|
+      @answer << [Macroregion.find(macroregion_id), Address.do_not_calls(macroregion_id)]
+    end
+
+    @answer
+
+  end
+
+  # Retorna todas as designações ainda não completas do usuário
+  def assignments_to_do
+    @answer = []
+    
+    Assignment.where(user_id: self.id).each do |assignment|
+      assignment.concluded? ? nil : @answer << assignment
+    end
+
+    return @answer
+  end
 end
