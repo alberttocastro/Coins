f9e8d91c911457102d2802d4d6fdcc603b9a6f9a
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-08-30 08:20:11 -0300

    Adicionando id da macroregião no card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180830092247) do
+ActiveRecord::Schema.define(version: 20180830111833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180830092247) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "macroregion_id"
   end
 
   create_table "groups", force: :cascade do |t|
