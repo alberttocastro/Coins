a0b903cc4a95832e473e7c8ff2ee23b77ee92cb5
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2018-09-12 21:36:04 -0300

    Iniciando overview

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,13 @@
 class Card < ApplicationRecord
     has_many :addresses
     has_many :assignments
+
+    def description
+        @answer = ""
+        self.addresses.each do |address|
+            @answer = @answer + "#{address.street}"
+        end
+      
+    end
+
 end
