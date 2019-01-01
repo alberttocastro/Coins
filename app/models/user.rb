054fc8d1f96aa20c80072af766f7ed19661103fd
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2019-01-01 20:40:37 -0300

    [bugfix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,33 +7,36 @@ class User < ApplicationRecord
   # Retorna todos os endereços que não devem ser batidos de acordo com a deisgnação da pessoa, no seguinte formato:
   # ["Macroregião",[Endereço 1, Endereço 2, Endereço 3]]
   def do_not_call_info
-    @answer = []
+    @do_not_call_info = []
 
     @macroregions = []
     self.assignments_to_do.each do |assignment|
-      assignment.card.addresses.each do |address|
-        @answer << address.macroregion_id
+      begin
+        assignment.card.addresses.each do |address|
+          @macroregions << address.macroregion_id
+        end
+      rescue => exception
+        puts "exception block"
       end
     end
 
     @macroregions = @macroregions.uniq
 
     @macroregions.each do |macroregion_id|
-      @answer << [Macroregion.find(macroregion_id), Address.do_not_calls(macroregion_id)]
+      @do_not_call_info << [Macroregion.find(macroregion_id), Address.do_not_calls(macroregion_id)]
     end
 
-    @answer
-
+    @do_not_call_info
   end
 
   # Retorna todas as designações ainda não completas do usuário
   def assignments_to_do
-    @answer = []
+    @assignments_to_do = []
     
     Assignment.where(user_id: self.id).each do |assignment|
-      assignment.concluded? ? nil : @answer << assignment
+      assignment.concluded? ? nil : @assignments_to_do << assignment
     end
 
-    @answer === [] ? @answer : [@answer]
+    @assignments_to_do
   end
 end
