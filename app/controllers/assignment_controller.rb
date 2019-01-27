54bb0e59d9f8725b8b188aa254a69fd78dbed1fe
Alberto Castro Bandeira de Mello <albertto.castro@gmail.com>
2019-01-26 22:02:22 -0300

    Método para definir todas as designações dos irmãos que estão com o cartão como concluidas

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -5,4 +5,20 @@ class AssignmentController < ApplicationController
       render "assignments"
     end
 
+    # Declara todos as designações de um endereço como sendo encerradas
+    def finish_all_card_assignments
+      @params = params.permit(:card_id)
+
+      begin
+        Assignment.where(card_id: @params[:card_id], date: nil).each do |assignment|
+          assignment.update(date: Date.today)
+        end
+      rescue => exception
+        puts "An error ocurred while trying to get assignment done"
+        puts exception
+      end
+
+      redirect_to assignments_path
+    end
+
 end
