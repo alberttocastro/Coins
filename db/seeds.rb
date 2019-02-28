0970513f6343f9c450d06101c6f517b5e612e22f
Albertto Castro <albertto.programmer@gmail.com>
2019-02-28 20:53:48 -0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,47 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Macroregion.create([
-#     {macroreg: Faker::Address.community},
-#     {macroreg: Faker::Address.community},
-#     {macroreg: Faker::Address.community},
-#     {macroreg: Faker::Address.community},
-#     {macroreg: Faker::Address.community}
-# ])
-
-# Card.create([
-#     {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.first},
-#     {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.second},
-#     {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.third},
-#     {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.fourth},
-#     {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.fifth},
-# ])
-
-# Adrstype.create([
-#     {typename:"type"}
-# ])
-
-# Nationality.create([
-#     {country: Faker::Address.country},
-#     {country: Faker::Address.country}
-# ])
-
-# Idiom.create(
-#     {language: "language"}
-# )
-
-# Group.create([
-#     {number: Faker::Number.digit, conductor: 1, helper: 1},
-#     {number: Faker::Number.digit, conductor: 1, helper: 1},
-#     {number: Faker::Number.digit, conductor: 1, helper: 1},
-#     {number: Faker::Number.digit, conductor: 1, helper: 1},
-#     {number: Faker::Number.digit, conductor: 1, helper: 1}
-# ])
-[1..6].each do |d|
-Address.create([
-    {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.first.id},
-    {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.second.id},
-    {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.third.id},
-    {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.fourth.id}
+Macroregion.create([
+    {macroreg: Faker::Address.community},
+    {macroreg: Faker::Address.community},
+    {macroreg: Faker::Address.community},
+    {macroreg: Faker::Address.community},
+    {macroreg: Faker::Address.community}
 ])
-end
+
+Card.create([
+    {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.first},
+    {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.second},
+    {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.third},
+    {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.fourth},
+    {number: Faker::Number.between(0, 100), name: Faker::Address.street_name, macroregion: Macroregion.fifth},
+])
+
+Adrstype.create([
+    {typename:"type"}
+])
+
+Nationality.create([
+    {country: Faker::Address.country},
+    {country: Faker::Address.country}
+])
+
+Idiom.create(
+    {language: "language"}
+)
+
+Group.create([
+    {number: Faker::Number.digit, conductor: 1, helper: 1},
+    {number: Faker::Number.digit, conductor: 1, helper: 1},
+    {number: Faker::Number.digit, conductor: 1, helper: 1},
+    {number: Faker::Number.digit, conductor: 1, helper: 1},
+    {number: Faker::Number.digit, conductor: 1, helper: 1}
+])
+# [1..6].each do |d|
+# Address.create([
+#     {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.first.id},
+#     {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.second.id},
+#     {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.third.id},
+#     {neighborhood: Faker::Address.community,street: Faker::Address.street_address, nationality_id: Nationality.first.id, idiom_id: Idiom.first, name: Faker::TvShows::Friends.character, is_valid?: true, is_visitable?: true, adrstype: Adrstype.first, macroregion_id: Macroregion.first.id, group_id: Group.first.id, card_id: Card.fourth.id}
+# ])
+# end
