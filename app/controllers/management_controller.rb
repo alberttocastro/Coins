40b955e7385b01b6058a8cd494cf561855152434
Albertto Castro <albertto.programmer@gmail.com>
2019-03-02 12:55:11 -0300

    Método para aceitar sugestão de endereço

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -5,13 +5,16 @@ class ManagementController < ApplicationController
 
   # Ação para incluir uma sugestão de endereço como endereço no banco de dados
   def accept_suggestion
-    @params = params.permit(:id, :street, :neighborhood, :name, :comments, :utf8, :authenticity_token, :commit)
+    @params = params.permit(:id, :street, :neighborhood, :name, :comments, :adrstype_id, :card_id, :group_id, :macroregion_id, :utf8, :authenticity_token, :commit)
 
     # Remove o "id" da hash e criar um endereço com o restante.
     # Caso a criação do endereço funcione, destroi o endereço sugerido
-    if Address.create(@params.except(:id, :utf8, :authenticity_token, :commit))
+    @created = Address.new(@params.except(:id, :utf8, :authenticity_token, :commit))
+    if @created.save
       SuggestedAddress.find(@params[:id]).destroy
     end
+
+    puts @created.errors.messages
 
     redirect_to management_path
   end
