9284bf48a663e3a7ef8276548cf11ed566fda5b5
Albertto Castro <albertto.programmer@gmail.com>
2019-03-05 12:35:55 -0300

    Testes do controlador HOME

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get view" do
-    get home_view_url
+  test "deve ver pagina home" do
+    get home_url
+    assert_response :success
+  end
+
+  test "deve ver pagina login" do
+    get login_url
+    assert_response :success
+  end
+
+  test "deve ver pagina view" do
+    get view_url(address_id: 298486374)
+    assert_response :success
+  end
+
+  test "deve ver pagina edit" do
+    get edit_url(id: 1)
     assert_response :success
   end
 
