5c0cf44d3158f14ba4586adea061adba4ff803a5
Albertto Castro <albertto.programmer@gmail.com>
2019-03-05 12:47:20 -0300

    Teste do controlador de designação

--- test/controllers/assignment_controller_test.rb
+++ test/controllers/assignment_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AssignmentControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve ver pagina assignment" do
+    get assignments_url
+    assert_response :success
+  end
 end
