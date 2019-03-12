70a98bb9132c1e1a9eff2aab5aaefe377fbad3aa
Albertto Castro <albertto.programmer@gmail.com>
2019-03-12 18:47:01 -0300

    Teste do model de designacao

--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve declarar concluido" do
+    # TODO: Fazer
+  end
+
+  test "deve verificar se esta concluido" do
+    # TODO: Fazer
+  end
+
+  test "deve retornar todos os enderecos concluidos" do
+    # TODO: Fazer
+  end
 end
