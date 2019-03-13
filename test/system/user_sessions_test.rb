2b28408eddec3ecc2da81c8b506502a07b5f2688
Albertto Castro <albertto.programmer@gmail.com>
2019-03-13 08:21:40 -0300

    Pequeno primeiro teste de sistema

--- test/system/user_sessions_test.rb
+++ test/system/user_sessions_test.rb
@@ -1,6 +1,11 @@
 require "application_system_test_case"
 
 class UserSessionsTest < ApplicationSystemTestCase
+  test "deve existir label de user" do
+    visit root_url
+    assert_selector "label", text: "User"
+  end
+  
   # test "visiting the index" do
   #   visit user_sessions_url
   #
