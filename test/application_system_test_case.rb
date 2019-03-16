aa4347fc8b1337f8d35a5527e97eba9033442342
Albertto Castro <albertto.programmer@gmail.com>
2019-03-16 18:38:06 -0300

    Código para rodar o selenium webdriver remotamente (funcionando)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,14 @@
 require "test_helper"
+#require 'selenium-webdriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  #driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome
+  Capybara.javascript_driver = :selenium_remote_chrome
+  Capybara.register_driver "selenium_remote_chrome".to_sym do |app|
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://localhost:4444/wd/hub", desired_capabilities: caps)
+  end
+
+  driven_by :selenium_remote_chrome
 end
