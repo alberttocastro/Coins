bbd2c460e3a1d70c5e23fcc7109b7b6dc861365b
Albertto Castro <albertto.programmer@gmail.com>
2019-03-20 21:28:54 -0300

    Método para retornar os publicadores que não estão com usuários

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,4 +2,15 @@ class Publisher < ApplicationRecord
     has_many :addresses
     belongs_to :group
     belongs_to :macroregion
+
+    def self.without_user
+        @answer = []
+        Publisher.all.each do |publisher|
+            User.where(publisher_id: publisher.id).length == 0 ? @answer << publisher : nil
+        end
+
+        # o retorno
+        @answer
+    end
+    
 end
