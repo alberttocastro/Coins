ac1393d96b699301f6f11e25880db9131cb6b3e3
Albertto Castro <albertto.programmer@gmail.com>
2019-03-20 21:49:41 -0300

    Teste da função de retornar publicadores sem usuário

--- test/models/publisher_test.rb
+++ test/models/publisher_test.rb
@@ -4,9 +4,13 @@ class PublisherTest < ActiveSupport::TestCase
   test "deve retornar publicadores que nao estao associados com usuarios" do 
     @answer = true
     Publisher.without_user.each do |publisher|
-      User.where(publisher_id: publisher.id) == 0 ? @answer = @answer && true : @answer = false
+      User.where(publisher_id: publisher.id).length == 0 ? @answer = @answer && true : @answer = false
     end
 
     assert(@answer, "Retorna publicador que já está associado")
+
+    # TODO: Criar teste que enviar o id do publicador que está associado a um usuário, e retorna ele como primeiro, depois retorna todos os outros que não tem usuário associado
   end
+
+
 end
