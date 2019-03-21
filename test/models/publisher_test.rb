f3c63b212357e6d585862c0d466a021549b1060c
Albertto Castro <albertto.programmer@gmail.com>
2019-03-20 21:24:00 -0300

    Teste para retornar todos os publicadores que não estão listados com usuário

--- test/models/publisher_test.rb
+++ test/models/publisher_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PublisherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve retornar publicadores que nao estao associados com usuarios" do 
+    @answer = true
+    Publisher.without_user.each do |publisher|
+      User.where(publisher_id: publisher.id) == 0 ? @answer = @answer && true : @answer = false
+    end
+
+    assert(@answer, "Retorna publicador que já está associado")
+  end
 end
