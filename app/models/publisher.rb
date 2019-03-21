f80f5cdf43c3c8a1293f9dab58aba8a3e8740618
Albertto Castro <albertto.programmer@gmail.com>
2019-03-20 21:49:23 -0300

    Retornar todos os publicadores que não tem usuário associado

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -3,10 +3,16 @@ class Publisher < ApplicationRecord
     belongs_to :group
     belongs_to :macroregion
 
-    def self.without_user
+    # Retorna todos os publicadores que estão sem usuário registrado
+    # Caso envie um id de usuário, como no caso de um usuário que já tem o publicador cadastrado, retorna o usuário recebido na primeira posição, e em seguida os outros que ainda não tem publicador
+    def self.without_user(publisher_id = "")
         @answer = []
         Publisher.all.each do |publisher|
             User.where(publisher_id: publisher.id).length == 0 ? @answer << publisher : nil
+        end
+
+        if publisher_id != ""
+            @answer.unshift(Publisher.find(publisher_id))
         end
 
         # o retorno
