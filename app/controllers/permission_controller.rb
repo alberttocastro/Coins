909b125346c60d8e4660d9018608b159a3914adf
Albertto Castro <albertto.programmer@gmail.com>
2019-03-21 15:04:47 -0300

    Método do controlador para associar o usuário ao publisher

--- app/controllers/permission_controller.rb
+++ app/controllers/permission_controller.rb
@@ -1,4 +1,15 @@
 class PermissionController < ApplicationController
   def use_permission_management
   end
+
+  def put_permissions
+    User.all.each do |user|
+      if !params["publisher_#{user.id}"].empty? && params["publisher_#{user.id}"] != "0" && params["publisher_#{user.id}"] != 0
+        user.update(publisher_id: params["publisher_#{user.id}"].to_i)
+      end
+    end
+
+    redirect_to permission_management_path
+  end
+  
 end
