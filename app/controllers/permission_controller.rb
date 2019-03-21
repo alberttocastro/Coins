c18fa334112ed223674b4196dc17999378e4e921
Albertto Castro <albertto.programmer@gmail.com>
2019-03-21 18:21:23 -0300

    Remover publicador de usuário

--- app/controllers/permission_controller.rb
+++ app/controllers/permission_controller.rb
@@ -4,12 +4,16 @@ class PermissionController < ApplicationController
 
   def put_permissions
     User.all.each do |user|
-      if !params["publisher_#{user.id}"].empty? && params["publisher_#{user.id}"] != "0" && params["publisher_#{user.id}"] != 0
+      if !params["publisher_#{user.id}"].empty? && params["publisher_#{user.id}"] != "0" && params["publisher_#{user.id}"] != 0 && User.where(publisher_id: params["publisher_#{user.id}"].to_i).length == 0
         user.update(publisher_id: params["publisher_#{user.id}"].to_i)
       end
     end
 
     redirect_to permission_management_path
   end
-  
+
+  def remove_publisher
+    User.find(params[:id]).update(publisher_id: nil)
+    redirect_to permission_management_path
+  end  
 end
