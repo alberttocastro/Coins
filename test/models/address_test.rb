b41253ec4c1096bacaaf5383b3b5ff3731ed338a
Albertto Castro <albertto.programmer@gmail.com>
2019-04-04 16:34:11 -0300

    Teste para criar endereço sem precisar associar a um card

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -8,4 +8,10 @@ class AddressTest < ActiveSupport::TestCase
   test 'retorna se o endereco ja foi feito' do
     # TODO: fazer o teste 
   end
+
+  test 'cria endereço sem card' do
+    @address = Address.new(neighborhood: "Bairro", street:"Rua tal", name:"Nome da pessoa", is_valid?: true, is_visitable?: true, frequence:1, adrstype: Adrstype.first, macroregion: Macroregion.first, group: Group.first)
+
+    assert(@address.save, "Cartão não está sendo criado sem card")
+  end
 end
